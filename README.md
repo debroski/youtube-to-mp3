32141d923c0ecd7003aa1bf82e0197f9290e393c
debroski <donizetedebroski@gmail.com>
2022-09-26 21:24:46 -0300

    Ajustes Scripts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # youtube-to-mp3
+
+Usando a lista de URLs, o script baixa os videos no formato MP4 e converte para mp3.
+
--- testes.py
+++
@@ -1,9 +0,0 @@
-# read arquivos
-from os import lseek
-
-
-with open("lista_de_musicas.txt", "r") as music_list:
-    line = music_list.readlines()
-
-for url in line:
-     print(url)
